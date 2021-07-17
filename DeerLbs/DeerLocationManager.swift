3acc161cdf89f6a286d58d501aff22030caac6de
快乐的小梁同学 <350442340@qq.com>
2021-07-17 18:35:35 +0800

    lz-update

--- DeerLbs/DeerLocationManager.swift
+++ DeerLbs/DeerLocationManager.swift
@@ -24,6 +24,7 @@ public class DeerLocationManager: NSObject  {
         $0.locationTimeout = 2
         $0.reGeocodeTimeout = 2
     }
+    private var sysLocationManager = CLLocationManager()
     
     public lazy var search = AMapSearchAPI().then {
         $0.delegate = self
@@ -41,7 +42,10 @@ public class DeerLocationManager: NSObject  {
         case user: return "a4951ca38eae763d9c76dace4139191d"//用户端
         case doctor: return "961c7f2a814abcd524fc5ef5d19f7105"//医生端
         case "com.yce.houseDeer02": return "466719370590bc9234d811cb188bba52"
-        default: return ""
+        case "com.yce.nurse02": return "1950a6bd2ca74155ee9809a8bea944d0"
+        default:
+            assert(false, "请输入正常的key")
+            return ""
         }
     }
     
@@ -54,13 +58,14 @@ public class DeerLocationManager: NSObject  {
         
         MAMapView.metalEnabled = true
         //先定位一次
-//        DeerLocationManager.shared.requestLocation { _, _ in
-//            
-//        }
+        DeerLocationManager.shared.requestLocation { _, r, _ in
+            
+        }
     }
     
     /// 开启持续定位
     public func startUpdatingLocation() {
+        requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
     }
     
@@ -71,11 +76,16 @@ public class DeerLocationManager: NSObject  {
     
     /// 定一次位
     public func requestLocation(completion: @escaping ((DeerLocationManager, AMapLocationReGeocode?, CLLocation?) -> Void)) {
+        requestWhenInUseAuthorization()
         locationManager.requestLocation(withReGeocode: true) { [weak self] location, reGeocode, error in
             guard let self = self else { return }
             
             if let location = location {
                 self.location = location
+//                let request = AMapReGeocodeSearchRequest()
+//                request.location = AMapGeoPoint.location(withLatitude: CGFloat(location.coordinate.latitude), longitude: CGFloat(location.coordinate.longitude))
+//                request.requireExtension = true
+//                self.search.aMapReGoecodeSearch(request)
             }
             
             if let reGeocode = reGeocode {
@@ -110,6 +120,30 @@ public class DeerLocationManager: NSObject  {
         
         self.distanceSearchCallback = completion
     }
+    
+    
+    func requestWhenInUseAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        
+        case .notDetermined://用户没有选择是否要使用定位服务（弹框没选择，或者根本没有弹框）
+            sysLocationManager.requestWhenInUseAuthorization()
+            print("位置授权： notDetermined")
+        case .restricted: //定位服务授权状态受限制，可能由于活动限制了定位服务，并且用户不能改变当前的权限，这个状态有可能不是用户拒绝的，但是也有可能是用户拒绝的
+            sysLocationManager.requestWhenInUseAuthorization()
+            print("位置授权： restricted")
+        case .denied: ////用户在设置中关闭定位功能，或者用户明确的在弹框之后选择禁止定位
+            sysLocationManager.requestWhenInUseAuthorization()
+            print("位置授权： denied")
+        case .authorizedAlways:
+            print("位置授权： authorizedAlways")
+        case .authorizedWhenInUse:
+            print("位置授权： authorizedWhenInUse")
+        @unknown default:
+            break
+        }
+    }
+
+
 }
 
 //MARK: - 定位代理
@@ -137,7 +171,14 @@ extension DeerLocationManager: AMapSearchDelegate {
     }
     
     // 逆地理编码（坐标转地址）
-
+    public func onReGeocodeSearchDone(_ request: AMapReGeocodeSearchRequest!, response: AMapReGeocodeSearchResponse!) {
+     
+        if let regeocode =  response.regeocode  {
+            print("")
+        }
+        print("")
+    }
+    
     //  距离测量
     public func onDistanceSearchDone(_ request: AMapDistanceSearchRequest!, response: AMapDistanceSearchResponse!) {
         if response.results == nil {
